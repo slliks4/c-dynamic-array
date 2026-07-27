a9831b466b7859b2fb0a24e48ad4011cb2123a3f
scaramouche <skillsnwokolo372@gmail.com>
2026-07-27 01:54:31 -0230

    v0 implementation

--- src/main.c
+++ src/main.c
@@ -1,115 +1,110 @@
 #include "../include/unimplemented.h"
 #include <stdio.h>
 
-// Creating a dynamic array
+#define GROWTH_FACTOR 2
 
-void *init_array(size_t array_size)
+struct array
 {
-	void *array_p = malloc(array_size);
+	int count;
+	int capacity;
+};
 
-	if (array_p == NULL)
+struct array *init_array()
+{
+	struct array *arr = malloc(sizeof(*arr) + sizeof(int));
+
+	if (arr == NULL)
 	{
 		return NULL;
 	}
 
-	return array_p;
-};
+	arr->count = 0;
+	arr->capacity = 1;
 
-int array_len(int *array_header){
-    return *array_header;
+	return arr;
 }
 
-void array_append(int *array_header, int item){
-    size_t offset = sizeof(*array_header) + *array_header;
-    array_header[offset] = item;
+int array_len(struct array *arr)
+{
+	return arr->count;
 }
 
-int array_pop(){
-    UNIMPLEMENTED;
+int array_get(struct array *arr, int index)
+{
+	const int *data = (const int *)(arr + 1);
+	return data[index];
 }
 
-int array_remove(){
-    UNIMPLEMENTED;
-}
+int array_append(struct array **arr_pointer, int item)
+{
+	struct array *arr = *arr_pointer;
 
+	if (arr->count >= arr->capacity)
+	{
+		int new_capacity = arr->capacity * GROWTH_FACTOR;
+
+		struct array *new_arr = malloc(sizeof(*new_arr) + (sizeof(int) * new_capacity));
+
+		if (new_arr == NULL)
+		{
+			return -1;
+		}
+
+		new_arr->count = arr->count;
+		new_arr->capacity = new_capacity;
+
+		int *new_data = (int *)(new_arr + 1);
+
+		for (int i = 0; i < arr->count; ++i)
+		{
+			new_data[i] = array_get(arr, i);
+		}
+
+		free(arr);
+		arr = new_arr;
+		*arr_pointer = new_arr;
+	}
+
+	int *data = (int *)(arr + 1);
+	data[arr->count] = item;
+
+	++arr->count;
+
+	return 0;
+}
 
 int main()
 {
-	// Initialize Dynamic Array
-	int capacity = 10;
-	int array_length = 2;
+	struct array *my_array = init_array();
 
-    // Accounting for array header
-	size_t array_size = sizeof(array_length) + (sizeof(int) * capacity);
-	size_t array_size_char = sizeof(array_length) + (sizeof(char) * capacity);
-
-    // Growth factor for increasing the array size when maxed
-	const double growth_factor = 2;
-
-	// Create dynamic array
-	int *array_header = init_array(array_size);
-	int *array_header_char = init_array(array_size_char);
-
-	// Incase operating system denies like why would it do that
-	if (array_header == NULL || array_header_char == NULL)
+	if (my_array == NULL)
 	{
 		return 1;
 	}
 
-	// Let's create little meta data for our array that is array[0]
-	// The first element in our array
-	*array_header = array_length;
+	printf("array len == %d\n", array_len(my_array));
+	printf("array capacity = %d\n", my_array->capacity);
 
-	// *array_header_char = array_length;
-    // printf("%d\n", array_len(array_header));
+	for (int i = 0; i < 10; ++i)
+	{
+		if (array_append(&my_array, i) != 0)
+		{
+			free(my_array);
+			return 1;
+		};
+	}
 
-    array_append(array_header, 2);
+	printf("\nValues:\n");
 
-    for (int i=0; i < *array_header; ++i){
-        printf("%d\n", array_header[i]);
-    }
+	for (int i = 0; i < array_len(my_array); ++i)
+	{
+		printf("%d\n", array_get(my_array, i));
+	}
 
-	// Let's Create initialize our offset
-	// Keeping track of the array header
-	// size_t current_offset = sizeof(*array_header) + (sizeof(int) * array_length);
-	// size_t current_offset_char = sizeof(*array_header_char) + (sizeof(char) * array_length);
+	printf("array len == %d\n", array_len(my_array));
+	printf("array capacity = %d\n", my_array->capacity);
 
-    (void) growth_factor;
-
-    // printf("%zu\n", current_offset);
-    // printf("%zu\n", current_offset_char);
+	free(my_array);
 
 	return 0;
-
-
-    // assert(current_offset % *(array_header + 1) == 0);
-    // int current_position = current_offset / *(array_header + 1);
-
-    // printf("%zu\n", current_offset);
-    // printf("%zu\n", current_offset_char);
-
-	// // Dummy append to the array for fun I guess
-	// for (int i = 0; i < 100; ++i)
-	// {
-	// 	// check if you can actually add
-	// 	// Like the array can be filled up STOP THAT!
-	// 	if ((sizeof(i) * array_length) >= array_size)
-	// 	{
-	//            // int new_capacity = (capacity * growth_factor) + 1;
-	//            // int * bigger_array = init_array(array_size * new_capacity);
-	//            // *bigger_array = *array_p;
-	//            // for(int j = 0; j < capacity; ++j){
-	//            //     bigger_array[]
-	//            // }
-	// 		return 1;
-	// 	}
-	// 	++array_length;
-	// 	array_p[i + current_offset] = i;
-	//
-	//     printf("%d\n", *(array_p + i));
-	// }
-	//
-	//    (void) growth_factor;
-	//    (void) current_offset;
-	// printf("%zu\n", array_size);
 }
